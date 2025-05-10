8e0c9c1c50639df8833edd4c7b7a4e73a4c2d65b
Your Name <your.email@example.com>
2025-05-10 05:20:27 -0400

    Explicitly set basename in BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,9 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+
