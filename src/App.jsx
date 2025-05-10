ac155f6e6ca405f837fafda72ef781808b43bb03
LooneyLuna79 <207087670+LooneyLuna79@users.noreply.github.com>
2025-05-10 12:56:00 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// Import your page components
-import Home from './Home'; // Assuming you have a Home.jsx in the same directory
-import InPage from './InPage'; // Assuming you have InPage.jsx in the same directory
+
+import React from 'react';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} /> {/* Route for the root path */}
-        <Route path="/in" element={<InPage />} /> {/* Route for /in */}
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>This is a test from App.jsx</h1>
+    </div>
   );
 }
 
 export default App;
-
